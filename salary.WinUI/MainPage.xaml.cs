a4effaf1f666faf8b1a33dd352b25ea2a50b480a
Sharanya M Kumar <sharanyamohankumar15@gmail.com>
2026-09-12 09:00:55 +0530

    Add project files.

---
+++ salary.WinUI/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+using Microsoft.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace salary_WinUI
+{
+    /// <summary>
+    /// The main content page displayed inside the application window.
+    /// Add your UI logic, event handlers, and data binding here.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // TODO: Add your initialization logic here.
+        }
+    }
+}
